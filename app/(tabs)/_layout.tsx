be3da1d2fe503c25679f19e310c3042864a8e675
Felipe serpa <felipe.mago_skt@hotmail.com>
2026-02-23 18:53:12 -0300

    criaçao da tela da home

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,40 +1,44 @@
 import { Tabs } from "expo-router";
 import React from "react";
-
 import { HapticTab } from "@/components/haptic-tab";
 import { IconSymbol } from "@/components/ui/icon-symbol";
-import { Colors } from "@/constants/theme";
 import { useColorScheme } from "@/hooks/use-color-scheme";
+import { StatusBar } from "expo-status-bar";
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-      }}
-    >
-      <Tabs.Screen
-        name="home"
-        options={{
-          title: "home",
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="house.fill" color={color} />
-          ),
+    <>
+      <StatusBar style="auto" />
+
+      <Tabs
+        screenOptions={{
+          tabBarActiveTintColor: "#3894ac",
+          headerShown: true,
+          tabBarButton: HapticTab,
         }}
-      />
-      <Tabs.Screen
-        name="mapa"
-        options={{
-          title: "Explore",
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="paperplane.fill" color={color} />
-          ),
-        }}
-      />
-    </Tabs>
+      >
+        <Tabs.Screen
+          name="home"
+          options={{
+            title: "Home",
+            tabBarIcon: ({ color }) => (
+              <IconSymbol size={28} name="house.fill" color={color} />
+            ),
+          }}
+        />
+
+        <Tabs.Screen
+          name="mapa"
+          options={{
+            title: "Explore",
+            tabBarIcon: ({ color }) => (
+              <IconSymbol size={28} name="paperplane.fill" color={color} />
+            ),
+          }}
+        />
+      </Tabs>
+    </>
   );
 }
